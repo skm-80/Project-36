1ff4ab4322d1b397809fe8d62edac416cbc45ee8
skm-80 <54751352+skm-80@users.noreply.github.com>
2021-12-08 19:03:21 +0000

    Add files via upload

--- js/Question.js
+++ js/Question.js
@@ -5,31 +5,27 @@ class Question {
     this.input1 = createInput("Enter Your Name Here....");
     this.input2 = createInput("Enter Correct Option No..");
     this.button = createButton('Submit');
+    this.button2 = createButton('Reset');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
     this.option3 = createElement('h4');
     this.option4 = createElement('h4');
-    this.message = createElement("h2")
   }
 
   hide(){
     this.title.hide();
     this.input1.hide();
     this.button.hide();
+    //this.button2.hide();
     this.input2.hide();
-    this.message.hide();
   }
 
   display(){
     this.title.html("MyQuiz Game");
     this.title.position(350, 0);
 
-    this.question("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    this.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
     this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-    question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
-
     this.question.position(150, 80);
     this.option1.html("1: Everyone " );
     this.option1.position(150, 100);
@@ -43,22 +39,26 @@ class Question {
     this.input1.position(150, 230);
     this.input2.position(350, 230);
     this.button.position(290, 300);
+    this.button2.position(360, 300);
 
     this.button.mousePressed(()=>{
-      
-      this.message.html("Thank You, Your Answer Has Been Submitted");
-      this.message.position(350, 350);
-
-      this.message.html("Thank You, Your Answer Has Been Submitted");
-      this.message.position();
-
-      this.message("Thank You, Your Answer Has Been Submitted");
-      this.message(350, 350);
-
-      this.message.html("Thank You, Your Answer Has Been Submitted");
-      this.message.position(350);
-
-
+      this.title.hide();
+      this.input1.hide();
+      this.input2.hide();
+      this.button.hide();
+      contestant.name = this.input1.value();
+      contestant.answer = this.input2.value();
+      contestantCount+=1;
+      contestant.index = contestantCount;
+      contestant.update();
+      contestant.updateCount(contestantCount);
     });
+
+    this.button2.mousePressed(()=>{
+      contestant.updateCount(0)
+      quiz.update(0)
+      var ref = database.ref("contestants")
+      ref.remove()
+    })
   }
 }
